opendaylight/infrautils

Replace metrics.CloseableMetric with a more generic utils.Closeable
as suggested by Tom in I76ce2e1600e8f99eb735aac4c29c423cc01c59ee review.

Change-Id: Iba7bda3f51594764c21ba8d4dd7dc69f824f5125
Signed-off-by: Michael Vorburger <vorburger@redhat.com>

metrics/api/src/main/java/org/opendaylight/infrautils/metrics/Counter.java
@@ -7,7 +7,9 @@
  */
 package org.opendaylight.infrautils.metrics;
 
-public interface Counter extends CloseableMetric {
+import org.opendaylight.infrautils.utils.Closeable;
+
+public interface Counter extends Closeable {
 
     default void increment() {
         increment(1);

metrics/api/src/main/java/org/opendaylight/infrautils/metrics/Meter.java
@@ -7,10 +7,12 @@
  */
 package org.opendaylight.infrautils.metrics;
 
+import org.opendaylight.infrautils.utils.Closeable;
+
 /**
  * A meter metric which measures throughput.
  */
-public interface Meter extends CloseableMetric {
+public interface Meter extends Closeable {
 
     /**
      * Mark the occurrence of an event.

metrics/api/src/main/java/org/opendaylight/infrautils/metrics/Timer.java
@@ -7,13 +7,14 @@
  */
 package org.opendaylight.infrautils.metrics;
 
+import org.opendaylight.infrautils.utils.Closeable;
 import org.opendaylight.infrautils.utils.function.CheckedCallable;
 import org.opendaylight.infrautils.utils.function.CheckedRunnable;
 
 /**
  * A timer metric which aggregates timing durations.
  */
-public interface Timer extends CloseableMetric {
+public interface Timer extends Closeable {
 
     /**
      * Times and records the duration of event which returns a value.

metrics/impl/src/main/java/org/opendaylight/infrautils/metrics/internal/MetricProviderImpl.java
@@ -12,8 +12,8 @@ import com.codahale.metrics.MetricRegistry;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import javax.annotation.PreDestroy;
 import javax.inject.Singleton;
-import org.opendaylight.infrautils.metrics.CloseableMetric;
 import org.opendaylight.infrautils.metrics.MetricProvider;
+import org.opendaylight.infrautils.utils.Closeable;
 import org.opendaylight.infrautils.utils.function.CheckedCallable;
 import org.opendaylight.infrautils.utils.function.CheckedRunnable;
 import org.ops4j.pax.cdi.api.OsgiServiceProvider;
@@ -170,7 +170,7 @@ public class MetricProviderImpl implements MetricProvider {
         }
     }
 
-    private abstract class CloseableMetricImpl implements CloseableMetric {
+    private abstract class CloseableMetricImpl implements Closeable {
         private volatile boolean isClosed = false;
         private final String id;
 

opendaylight/serviceutils

metrics/api/src/main/java/org/opendaylight/infrautils/metrics/Counter.java
@@ -7,13 +7,15 @@
  */
 package org.opendaylight.infrautils.metrics;
 
+import org.opendaylight.infrautils.utils.UncheckedCloseable;
+
 /**
  * Counter metric, which is a simple incrementing and decrementing number.
  *
  * <p>Note that if you find you only use its <tt>increase()</tt> and never <tt>decrease()</tt>
  * methods, then you probably want to use {@link Meter} with <tt>mark()</tt> instead of this.
  */
-public interface Counter extends CloseableMetric {
+public interface Counter extends UncheckedCloseable {
 
     default void increment() {
         increment(1);

metrics/api/src/main/java/org/opendaylight/infrautils/metrics/Meter.java
@@ -7,13 +7,15 @@
  */
 package org.opendaylight.infrautils.metrics;
 
+import org.opendaylight.infrautils.utils.UncheckedCloseable;
+
 /**
  * Meter metric, which measures throughput.
  *
  * <p>Note that this with <tt>mark()</tt> measures the rate at which a set of events occur;
  * whereas {@link Counter} is for things which will <tt>increase()</tt> - and can also <tt>decrease()</tt>.
  */
-public interface Meter extends CloseableMetric {
+public interface Meter extends UncheckedCloseable {
 
     /**
      * Mark the occurrence of an event.

metrics/api/src/main/java/org/opendaylight/infrautils/metrics/Timer.java
@@ -7,13 +7,14 @@
  */
 package org.opendaylight.infrautils.metrics;
 
+import org.opendaylight.infrautils.utils.UncheckedCloseable;
 import org.opendaylight.infrautils.utils.function.CheckedCallable;
 import org.opendaylight.infrautils.utils.function.CheckedRunnable;
 
 /**
  * A timer metric which aggregates timing durations.
  */
-public interface Timer extends CloseableMetric {
+public interface Timer extends UncheckedCloseable {
 
     /**
      * Times and records the duration of event which returns a value.

metrics/impl/src/main/java/org/opendaylight/infrautils/metrics/internal/MetricProviderImpl.java
@@ -28,8 +28,8 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.lang.management.ManagementFactory;
 import javax.annotation.PreDestroy;
 import javax.inject.Singleton;
-import org.opendaylight.infrautils.metrics.CloseableMetric;
 import org.opendaylight.infrautils.metrics.MetricProvider;
+import org.opendaylight.infrautils.utils.UncheckedCloseable;
 import org.opendaylight.infrautils.utils.function.CheckedCallable;
 import org.opendaylight.infrautils.utils.function.CheckedRunnable;
 import org.ops4j.pax.cdi.api.OsgiServiceProvider;
@@ -248,7 +248,7 @@ public class MetricProviderImpl implements MetricProvider {
         }
     }
 
-    private abstract class CloseableMetricImpl implements CloseableMetric {
+    private abstract class CloseableMetricImpl implements UncheckedCloseable {
         private volatile boolean isClosed = false;
         private final String id;
 